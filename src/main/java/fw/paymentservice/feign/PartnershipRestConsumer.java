ebd4a0fc7a986a391418681e27ead5c6bdd3232e
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-05-25 14:55:31 +0200

    webhook

--- src/main/java/fw/paymentservice/feign/PartnershipRestConsumer.java
+++ src/main/java/fw/paymentservice/feign/PartnershipRestConsumer.java
@@ -1,8 +1,12 @@
 package fw.paymentservice.feign;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@FeignClient(name="fw-partnership-service", url="${app.services.partnership.url}")
 public interface PartnershipRestConsumer {
+
     @PostMapping("/api/partnership/validate")
     void validatePartnership(Long partnershipId);
+
 }
